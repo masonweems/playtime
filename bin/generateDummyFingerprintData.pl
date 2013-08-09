eae5c5398e7f2fa5509676e60b767fdf6cb5ff8e
Mason Weems <mason@thymetech.com>
2013-08-09 05:30:22 -0700

    now generating dummy finger print data

--- bin/generateDummyFingerprintData.pl
+++ bin/generateDummyFingerprintData.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# generateDummyFingerprintData
+# generateummyFingerprintData
 
 use strict;
 
@@ -18,6 +18,8 @@ exit(&main($argc, @ARGV));
 # main
 ###############################
 
+my $INFO = 1;
+
 sub main {
 
     my ($argc, @argv) = @_;
@@ -30,8 +32,32 @@ sub main {
     my $inputSeed = $argv[1];
 
     srand($inputSeed);
-    my $firstRandom = rand();
-    print STDOUT  qq/INFO: Generate random fingerprint scannner output data based on seed=$inputSeed. First random = $firstRandom\n/;
+    my $firstRandom = int(rand(0xffffffff+1));
+    my $firstRandomInBinary = sprintf("%032b", $firstRandom);
+    my $firstRandomInHex = sprintf("%08x", $firstRandom);
+    $INFO && print STDOUT qq/INFO: Generate random fingerprint scannner output data based on seed=$inputSeed.\n/;
+    $INFO && print STDOUT qq/INFO: First random = $firstRandom (0x$firstRandomInHex, $firstRandomInBinary)\n/;
+
+    generateDummyFingerprintData(12, 24);
+}
+
+sub generateDummyFingerprintData {
+
+    my ($sensorWidth, $sensorHeight) = @_;
+
+    $INFO && print STDOUT qq/INFO: generateDummyFingerprintData\n/;
+
+    for (my $i=0; $i<$sensorHeight; $i++) {
+	my $lineToPrint = "";
+	for (my $j=0; $j<$sensorWidth; $j++) {
+	    my $randHex = sprintf("%08x", int(rand(0xffffffff+1)));
+	    $lineToPrint .= "0x".$randHex.",";
+	    if ($j == $sensorWidth-1) {
+		chop($lineToPrint);
+            }
+	}
+	print STDOUT $lineToPrint."\n";
+    }
 
 }
 
