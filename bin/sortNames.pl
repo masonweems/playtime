faab93c924e84770c496e0171f2d4ddb0c45e580
Mason Weems <mason@thymetech.com>
2013-08-09 04:07:54 -0700

    now sorting by last name but with errors

--- bin/sortNames.pl
+++ bin/sortNames.pl
@@ -13,5 +13,15 @@ while (<>) {
 	my $firstName = $1;
 	my $lastName = $2;
 	print "INFO: Found $firstName $lastName\n";
+        $firstNameForLastName{$lastName} = $firstName;
+	push(@lastNames, $lastName);
+    } else {
+	print "ERROR: Could not parse line: $_";
     }
+}
+
+my @sortedLastNames = sort(@lastNames);
+
+foreach my $sortedLastName (@sortedLastNames) {
+    print "$firstNameForLastName{$sortedLastName} $sortedLastName\n";
 }
